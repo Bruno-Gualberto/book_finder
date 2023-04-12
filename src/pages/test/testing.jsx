52832cf83468b67ec447bf9efef0e818d0bb1fe7
BrunoGualberto <bruno.gualberto@audibene.de>
2023-04-12 18:53:54 +0200

    first commit

---
+++ src/pages/test/testing.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Testing() {
+  return (
+    <div>
+      <h1>I'm the Testing Component!!</h1>
+      <Link href="/" >Take me Home!</Link>
+    </div>
+  )
+}
