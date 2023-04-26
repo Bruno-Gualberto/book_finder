e3406599579db120cd238632bf7c601637285488
Bruno-Gualberto <brunofgualberto@gmail.com>
2023-04-26 09:17:44 +0200

    add search field

--- src/components/CardsList/index.jsx
+++ src/components/CardsList/index.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react'
 import BookCard from '../BookCard'
 import styles from './CardsList.module.css'
 
 export default function CardsList () {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const handleSearch = () => {
+    console.log(searchValue);
+  }
+
   return (
     <div>
-      <input type="text" />
-      <div className={styles.cards__container}>
+      <div className={styles.searchContainer}>
+        <input 
+          className={styles.searchField}
+          onChange={handleChange} 
+          value={searchValue} 
+          type='text'
+          placeholder='Search for a book'
+        />
+        <button
+          className={styles.searchButton}
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
+      <div className={styles.cardsContainer}>
         <BookCard />
         <BookCard />
         <BookCard />
